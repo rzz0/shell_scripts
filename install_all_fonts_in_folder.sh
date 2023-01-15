9e17fd7fb50652332782329e1f8805338dfeec81
rzz0 <rsrampazzo@gmail.com>
2023-01-15 17:02:03 -0300

    [ Shell script ] install_all_font_in_folder.sh

--- install_all_fonts_in_folder.sh
+++ install_all_fonts_in_folder.sh
@@ -10,13 +10,10 @@
 # Set the path to the folder containing the fonts
 font_folder="/path/"
 
-# Use the find command with -print0 option
 find "$font_folder" -type f -name "*.ttf" -or -name "*.otf" -print0 | xargs -0 -I{}  sudo cp "{}" $HOME/.local/share/fonts/
 
-#change the permissions
 sudo chmod 644 $HOME/.local/share/fonts/*
 
-#update the font cache for specific directory
 sudo fc-cache -f -v -s $HOME/.local/share/fonts/
 
 echo "All fonts in $font_folder have been installed and font cache is updated"
