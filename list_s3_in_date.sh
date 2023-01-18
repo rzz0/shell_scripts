9ffaaf1ce3fd57c066b4f4a4c225066f83a9cba3
rzz0 <rsrampazzo@gmail.com>
2023-01-18 09:13:45 -0300

    [ Shell script ] ./list_s3_in_date.sh - add -h opt

--- list_s3_in_date.sh
+++ list_s3_in_date.sh
@@ -2,16 +2,18 @@
 
 #auth:rzz0
 # SCRIPT: return all files that were last modified today
-# Requires the AWS CLI
-# Instruction:
-# make the script executable: chmod +x list_s3_in_date.sh
-# run the script in linux terminal and add the parameters:
-# -n: bucket-name
-# -d: YYYY-MM-DD".
-# Example: ./list_s3_in_date.sh -n my-bucket-name -d 2022-12-31
-# The script will return the list in terminal and a csv file with the output.
 
-while getopts ":n:d:" opt; do
+usage() {
+    echo "Instruction:"
+    echo "make the script executable: chmod +x list_s3_in_date.sh"
+    echo "run the script in linux terminal and add the parameters:"
+    echo "-n: bucket-name"
+    echo "-d: YYYY-MM-DD"
+    echo "Example: ./list_s3_in_date.sh -n my-bucket-name -d 2022-12-31"
+    echo "The script will return the list in terminal and a csv file with the output."
+}
+
+while getopts ":n:d:h" opt; do
   case $opt in
     n)
       bucket="$OPTARG"
@@ -19,12 +21,18 @@ while getopts ":n:d:" opt; do
     d)
       date="$OPTARG"
       ;;
+    h)
+      usage
+      exit 0
+      ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
+      usage
       exit 1
       ;;
     :)
       echo "Option -$OPTARG requires an argument." >&2
+      usage
       exit 1
       ;;
   esac
@@ -32,6 +40,7 @@ done
 
 if [ -z "$bucket" ]; then
     echo "You must provide the -n option followed by the bucket name."
+    usage
     exit 1
 fi
 
